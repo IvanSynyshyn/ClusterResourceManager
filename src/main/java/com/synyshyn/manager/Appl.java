984e1f028f5cd61daf912af086a02526d1b21262
IvanSynyshyn <sg8l@meta.ua>
2016-12-28 22:30:31 +0200

    create maven project

---
+++ src/main/java/com/synyshyn/manager/Appl.java
@@ -0,0 +1,10 @@
+package com.synyshyn.manager;
+
+/**
+ * Created by Ivan Synyshyn on 28.12.2016.
+ */
+public class Appl {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
